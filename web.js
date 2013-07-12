c2861529da6e478f57f924bb7145604afd38d4cc
Fang Yang <53211yf@gmail.com>
2013-07-11 23:05:11 -0500

    updatess

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var tail = fs.readFileSync('index.html', 'utf8');
 app.get('/', function(request, response) {
   response.send(tail);
 });
-
+ 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
