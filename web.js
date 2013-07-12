a7a0072a8a6379b4ca4606d9f07a6d1bc5ec7219
Fang Yang <53211yf@gmail.com>
2013-07-11 22:49:54 -0500

    2 updatess

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var tail = fs.readFile("index.html", "utf8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(tail);
 });
 
 var port = process.env.PORT || 5000;
